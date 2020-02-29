ec76f8b75ebd9c0cf08d667402ba1c7cc119ae30
MyGatherBk <49818403+MyGatherBk@users.noreply.github.com>
2020-02-29 23:18:12 +0700

    Update addhost.sh

--- addhost.sh
+++ addhost.sh
@@ -4,32 +4,32 @@ if [ -d "/etc/squid/" ]; then
 elif [ -d "/etc/squid3/" ]; then
 	payload="/etc/squid3/payload.txt"
 fi
-tput setaf 7 ; tput setab 4 ; tput bold ; printf '%35s%s%-10s\n' "Adicionar Host ao Squid Proxy" ; tput sgr0
+tput setaf 7 ; tput setab 4 ; tput bold ; printf '%35s%s%-10s\n' "เพิ่มโฮสต์ไปที่ Squid Proxy" ; tput sgr0
 if [ ! -f "$payload" ]
 then
-	tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "Arquivo $payload não encontrado" ; tput sgr0
+	tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "ไฟล์ $payload ไม่พบ" ; tput sgr0
 	exit 1
 else
-	tput setaf 2 ; tput bold ; echo ""; echo "Domínios atuais no arquivo $payload:" ; tput sgr0
+	tput setaf 2 ; tput bold ; echo ""; echo "โดเมนปัจจุบันในไฟล์ $payload:" ; tput sgr0
 	tput setaf 3 ; tput bold ; echo "" ; cat $payload ; echo "" ; tput sgr0
-	read -p "Digite o domínio que deseja adicionar a lista: " host
+	read -p "ป้อนโดเมนที่คุณต้องการเพิ่มในรายการ: " host
 	if [[ -z $host ]]
 	then
-		tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "Você digitou um domínio vazio ou não existente!" ; echo "" ; tput sgr0
+		tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "คุณป้อนโดเมนว่างหรือไม่มีอยู่!" ; echo "" ; tput sgr0
 		exit 1
 	else
 		if [[ `grep -c "^$host" $payload` -eq 1 ]]
 		then
-			tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "O domínio $host já existe no arquivo $payload" ; echo "" ; tput sgr0
+			tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "โดเมน $host มีอยู่แล้วในไฟล์ $payload" ; echo "" ; tput sgr0
 			exit 1
 		else
 			if [[ $host != \.* ]]
 			then
-				tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "Você deve adicionar um domínio iniciando-o com um ponto!" ; echo "Por exemplo: .phreaker56.xyz" ; echo "Não é necessário adicionar subdomínios para domínios que já estão no arquivo" ; echo "Ou seja, não é necessário adicionar recargawap.claro.com.br" ; echo "se o domínio .claro.com.br já estiver no arquivo." ; echo ""; tput sgr0
+				tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "คุณต้องเพิ่มโดเมนที่ขึ้นต้นด้วยจุด!" ; echo "Por exemplo: .phreaker56.xyz" ; echo "ไม่จำเป็นต้องเพิ่มโดเมนย่อยสำหรับโดเมนที่มีอยู่แล้วในไฟล์" ; echo "นั่นคือไม่จำเป็นต้องเพิ่มการชาร์จ.claro.com.br" ; echo "ถ้าโดเมน .claro.com.br อยู่ในไฟล์แล้ว." ; echo ""; tput sgr0
 				exit 1
 			else
 				echo "$host" >> $payload && grep -v "^$" $payload > /tmp/a && mv /tmp/a $payload
-				tput setaf 7 ; tput setab 1 ; tput bold ; echo "" ; echo "Arquivo $payload atualizado, o domínio foi adicionado com sucesso:" ; tput sgr0
+				tput setaf 7 ; tput setab 1 ; tput bold ; echo "" ; echo "Arquivo $payload อัปเดต, เพิ่มโดเมนเรียบร้อยแล้ว:" ; tput sgr0
 				tput setaf 3 ; tput bold ; echo "" ; cat $payload ; echo "" ; tput sgr0
 				if [ ! -f "/etc/init.d/squid3" ]
 				then
@@ -38,7 +38,7 @@ else
 				then
 					service squid reload
 				fi	
-				tput setaf 7 ; tput setab 1 ; tput bold ; echo "" ; echo "O Proxy Squid Proxy foi recarregado com sucesso!" ; echo "" ; tput sgr0
+				tput setaf 7 ; tput setab 1 ; tput bold ; echo "" ; echo "โหลด Squid Proxy สำเร็จแล้ว!" ; echo "" ; tput sgr0
 				exit 1
 			fi
 		fi
